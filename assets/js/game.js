ad656598bf8512c57b0ed8e2781b69ec3a2472e2
ME <mebener44@gmail.com>
2021-03-31 18:02:55 -0700

    add enemy loop till death

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,10 @@ console.log(enemyNames[2])
   
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive 
+    while(enemyHealth > 0) {
+      // place fight function code block here . . .
+  
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -69,11 +71,14 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-};
+}
+}
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
 
 // run fight function to start game
 fight();
