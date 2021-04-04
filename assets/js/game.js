4bca56f9ba675a9a479dcc08d59f162e824de975
ME <mebener44@gmail.com>
2021-04-04 00:15:34 -0700

    fix fight skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,52 @@ var randomNumber = function(min, max) {
   
     return value;
   };
+
+  promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+   // var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping, but don't let them go into the negative
+    playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+    // return true if player wants to leave
+    return true;
+    return false;
+  }
+    // Conditional Recursive Function Call
+//if (promptFight === "" || promptFight === null) {
+    // repeat and execute as long as the enemy-robot is alive 
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        // ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+          // if true, leave fight by breaking loop
+          break;
+        }
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+  
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+  
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerInfo.playerMoney = playerInfo.money - 10;
+        shop();
+      }
+    }
+  }
+  
   
   // fight function (now with parameter for enemy's object holding name, health, and attack values)
   var fight = function(enemy) {
